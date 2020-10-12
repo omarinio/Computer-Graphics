971858a4173fd7f0585b38cbdfdf85a1c3ec805e
Omar Suleiman <jb18940@bristol.ac.uk>
2020-10-12 17:57:17 +0100

    single dimension greyscale interpolation

--- Week2/Week2/src/Interpolation.cpp
+++ Week2/Week2/src/Interpolation.cpp
@@ -20,9 +20,46 @@ std::vector<float> interpolateSingleFloats(float from, float to, int numVals) {
 	return result;
 }
 
+void draw(DrawingWindow &window, std::vector<float> weight) {
+	window.clearPixels();
+	for (size_t x = 0; x < window.width; x++) {
+		for (size_t y = 0; y < window.height; y++) {
+			float red = weight.at(x);
+			float green = weight.at(x);
+			float blue = weight.at(x);
+			uint32_t colour = (255 << 24) + (int(red) << 16) + (int(green) << 8) + int(blue);
+			window.setPixelColour(x, y, colour);
+		}
+	}
+}
+
+void update(DrawingWindow &window) {
+	// Function for performing animation (shifting artifacts or moving the camera)
+}
+
+void handleEvent(SDL_Event event, DrawingWindow &window) {
+	if (event.type == SDL_KEYDOWN) {
+		if (event.key.keysym.sym == SDLK_LEFT) std::cout << "LEFT" << std::endl;
+		else if (event.key.keysym.sym == SDLK_RIGHT) std::cout << "RIGHT" << std::endl;
+		else if (event.key.keysym.sym == SDLK_UP) std::cout << "UP" << std::endl;
+		else if (event.key.keysym.sym == SDLK_DOWN) std::cout << "DOWN" << std::endl;
+	} else if (event.type == SDL_MOUSEBUTTONDOWN) window.savePPM("output.ppm");
+}
+
 int main(int argc, char *argv[]) {
 	std::vector<float> result;
-	result = interpolateSingleFloats(2.2, 8.5, 7);
+	result = interpolateSingleFloats(255, 0, 320);
 	for(size_t i=0; i<result.size(); i++) std::cout << result[i] << " ";
 	std::cout << std::endl;
+
+	DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
+	SDL_Event event;
+	while (true) {
+		// We MUST poll for events - otherwise the window will freeze !
+		if (window.pollForInputEvents(event)) handleEvent(event, window);
+		update(window);
+		draw(window, result);
+		// Need to render the frame at the end, or nothing actually gets shown on the screen !
+		window.renderFrame();
+	}
 }
