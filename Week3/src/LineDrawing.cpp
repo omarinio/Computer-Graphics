e9990dd840041d5a929b0d2e22eb995da709802d
Omar <jb18940@bristol.ac.uk>
2020-10-23 14:48:17 +0100

    random triangles

--- Week3/src/LineDrawing.cpp
+++ Week3/src/LineDrawing.cpp
@@ -9,18 +9,24 @@
 #define WIDTH 320
 #define HEIGHT 240
 
-void drawLine(DrawingWindow &window, float fromX, float toX, float fromY, float toY, Colour colour) {
-	float xDiff = toX - fromX;
-	float yDiff = toY - fromY;
-	float numSteps = fmax(abs(xDiff), abs(yDiff));
-	float xStepSize = yDiff/numSteps;
+void drawLine(DrawingWindow &window, CanvasPoint from, CanvasPoint to, Colour colour) {
+	float xDiff = to.x - from.x;
+	float yDiff = to.y - from.y;
+	float numSteps = std::max(abs(xDiff), abs(yDiff));
+	float xStepSize = xDiff/numSteps;
 	float yStepSize = yDiff/numSteps;
 	for (float i = 0.0; i < numSteps; i++) {
-		float x = fromX + (xStepSize*i);
-		float y = fromY + (yStepSize*i);
+		float x = from.x + (xStepSize*i);
+		float y = from.y + (yStepSize*i);
 		uint32_t set = (255 << 24) + (colour.red << 16) + (colour.green << 8) + colour.blue;
 		window.setPixelColour(round(x), round(y), set);
 	}
+}
+
+void drawTriangle(DrawingWindow &window, CanvasTriangle triangle, Colour colour) {
+	drawLine(window, triangle[0], triangle[1], colour);
+	drawLine(window, triangle[1], triangle[2], colour);
+	drawLine(window, triangle[2], triangle[0], colour);
 }
 
 void draw(DrawingWindow &window, std::vector<float> weight) {
@@ -31,7 +37,7 @@ void draw(DrawingWindow &window, std::vector<float> weight) {
 			float green = weight.at(x);
 			float blue = weight.at(x);
 			uint32_t colour = (255 << 24) + (int(red) << 16) + (int(green) << 8) + int(blue);
-			window.setPixelColour(x, y, colour);
+			window.setPixelColour(round(x), round(y), colour);
 		}
 	}
 }
@@ -45,7 +51,15 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
 		if (event.key.keysym.sym == SDLK_LEFT) std::cout << "LEFT" << std::endl;
 		else if (event.key.keysym.sym == SDLK_RIGHT) std::cout << "RIGHT" << std::endl;
 		else if (event.key.keysym.sym == SDLK_UP) std::cout << "UP" << std::endl;
-		else if (event.key.keysym.sym == SDLK_DOWN) std::cout << "DOWN" << std::endl;
+		else if (event.key.keysym.sym == SDLK_DOWN) std::cout << "DOWN" << std::endl; 
+		else if (event.key.keysym.sym == SDLK_u) {
+			CanvasTriangle triangle = CanvasTriangle(CanvasPoint(rand()%window.width, rand()%window.height), 
+				CanvasPoint(rand()%window.width, rand()%window.height), CanvasPoint(rand()%window.width, rand()%window.height));
+
+			Colour colour = Colour(rand()%256, rand()%256, rand()%256);
+			drawTriangle(window, triangle, colour); 
+		}
+		
 	} else if (event.type == SDL_MOUSEBUTTONDOWN) window.savePPM("output.ppm");
 }
 
@@ -57,9 +71,6 @@ int main(int argc, char *argv[]) {
 		if (window.pollForInputEvents(event)) handleEvent(event, window);
 		update(window);
 
-		drawLine(window, 0, 160, 0, 120, Colour(255,0,0));
-		// draw(window, result);
-		// drawColour(window);
 		// Need to render the frame at the end, or nothing actually gets shown on the screen !
 		window.renderFrame();
 	}
